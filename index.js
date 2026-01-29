2b16caf5ba30d18d9666b617d5f2fea525799d60
Gabriel Mariano <gabriel.mariano12@hotmail.com>
2026-01-29 13:30:40 -0300

    Fix: Explicitly point to config file

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ server.listen(port, () => {
 
 // 2. Start Clawdbot Gateway
 console.log('Starting Clawdbot Gateway...');
-const clawdbot = spawn('npx', ['clawdbot', 'gateway', '--allow-unconfigured'], {
+const clawdbot = spawn('npx', ['clawdbot', 'gateway', '--config', 'clawdbot.json'], {
   stdio: 'inherit',
   shell: true,
   env: { ...process.env }
